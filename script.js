850ccb431203a4995e37c745850cbd318db1ac61
jamie <138041959+SciTechjamieklein@users.noreply.github.com>
2023-07-13 13:31:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let yam = 0;
+function yoy() {
+  yam++;
+  if (yam != 100) {
+    alert("yippee" + " you have " + yam + " yippees");
+  } else {
+    alert("...you can stop now... you have...100 yippees.");
+  }
+}
