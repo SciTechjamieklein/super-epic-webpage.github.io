68ebc913ec77d52e5d7054cbda02b38b989b7584
jamie <138041959+SciTechjamieklein@users.noreply.github.com>
2023-07-17 13:19:50 -0400

    this is technically useless because i have an internal script but whatever i should keep this one up to date too for consistency's sake

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let yam = 0;
+let icy = 0;
 function yoy() {
   yam++;
   if (yam != 100) {
@@ -6,4 +7,28 @@ function yoy() {
   } else {
     alert("...you can stop now... you have...100 yippees.");
   }
+}
+function convert() {
+  var txt;
+  if (confirm("convert 2 yippees to 1 icy?")) {
+    if (yam >= 2) {
+      yam = yam - 2;
+      icy++;
+      txt = "icy count = " + icy;
+    } else {
+      alert("you do not have enough yippees...");
+      if(icy>0){
+      txt = "icy count = "+icy;}else{
+        txt=":["''
+      }
+    }
+  } else {
+    alert("...");
+    if (icy > 0) {
+      txt = "icy count = " + icy;
+    } else {
+      txt = "...";
+    }
+  }
+  document.getElementById("convert").innerHTML = txt;
 }
