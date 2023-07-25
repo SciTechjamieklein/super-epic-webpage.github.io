023a30c3eefa68b0adb6308e09deee1a39c665cb
jamie <138041959+SciTechjamieklein@users.noreply.github.com>
2023-07-25 09:46:57 -0400

    keeping the external script up to date for absolutely no reason because it already exists in index.html but whatever

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ if (localStorage.getItem("ycount") == null) {
 } else {
   localStorage.getItem("ycount");
   yam = localStorage.getItem("ycount");
-  document.getElementById("yoy").innerHTML =
-    "yippee = " + localStorage.getItem("ycount");
 }
 //my code is a surreal nightmare. even i have no idea of whats going on.
 //IT WORKS!!! IT WORKS!!!
@@ -15,24 +13,24 @@ if (localStorage.getItem("icount") == null) {
 } else {
   localStorage.getItem("icount");
   icy = localStorage.getItem("icount");
-  document.getElementById("convert").innerHTML =
-    "icy = " + localStorage.getItem("icount");
 }
 if (localStorage.getItem("pcount") == null) {
   var ponum = 0;
   localStorage.setItem("pcount", ponum);
 } else {
   localStorage.getItem("pcount");
-  yam = localStorage.getItem("pcount");
-  document.getElementById("buyPo").innerHTML =
-    "potato = " + localStorage.getItem("pcount");
+  ponum = localStorage.getItem("pcount");
 }
 var ah;
 var txt;
 function yoy() {
   yam++;
   localStorage.setItem("ycount", yam);
-  document.getElementById("yoy").innerHTML ="yippee = " + localStorage.getItem("ycount");
+  document.getElementById("yoy").innerHTML =
+    "yippee = " + localStorage.getItem("ycount");
+  if (yam == null) {
+    yam = 0; //if this stops working i will actually scream
+  }
   if (yam != 100) {
     alert(
       "yippee" + " you have " + localStorage.getItem("ycount") + " yippees"
@@ -51,28 +49,27 @@ function yoy() {
 }
 function convert() {
   if (confirm("convert 2 yippees to 1 icy?")) {
-    if(ponum==null){
-      ponum=0;
-    }
-    if (icy == null){
-      icy=0;
+    if (icy == null) {
+      icy = 0;
     }
     if (yam >= 2) {
       yam = yam - 2;
       icy++;
       localStorage.setItem("icount", icy);
-      txt = "icy = " + localStorage.getItem("icount");
-      ah = "yippee = " + yam;
       localStorage.setItem("ycount", yam);
+      txt = "icy = " + icy;
+      ah = "yippee = " + yam;
+      document.getElementById("yoy").innerHTML = ah;
+      document.getElementById("convert").innerHTML = txt;
     } else {
       alert("you do not have enough yippees...");
       if (icy > 0) {
         localStorage.setItem("icount", icy);
-        txt = "icy = " + localStorage.getItem("icount");
+        txt = "icy = " + icy;
         ah = "yippee = " + yam;
       } else {
         localStorage.setItem("icount", icy);
-        txt = "icy = " + localStorage.getItem("icount");
+        txt = "icy = " + icy;
         ah = "yippee = " + yam;
       }
     }
@@ -80,47 +77,65 @@ function convert() {
     alert("...");
     if (icy > 0) {
       localStorage.setItem("icount", icy);
-      txt = "icy = " + localStorage.getItem("icount");
+      txt = "icy = " + icy;
       ah = "yippee = " + yam;
     } else {
       localStorage.setItem("icount", icy);
-      txt = "icy = " + localStorage.getItem("icount");
+      txt = "icy = " + icy;
       ah = "yippee = " + yam;
     }
   }
-  document.getElementById("yoy").innerHTML = ah;
-  document.getElementById("convert").innerHTML = txt;
 }
 function buyPo() {
   var pota;
   if (confirm("buy a potato for 1 icy?")) {
+    if (ponum == null) {
+      ponum = 0;
+    }
     if (icy >= 1) {
       --icy;
       localStorage.setItem("icount", icy);
       ponum++;
       localStorage.setItem("pcount", ponum);
-      pota = "potato = " + localStorage.getItem("pcount");
-      txt = "icy = " + localStorage.getItem("icount");
+      pota = "potato = " + ponum;
+      txt = "icy = " + icy;
+      document.getElementById("yoy").innerHTML = ah;
+      document.getElementById("convert").innerHTML = txt;
+      document.getElementById("buyPo").innerHTML = pota;
     } else {
       alert("you do not have enough icy...");
       if (icy > 0) {
-        pota = "potato = " + localStorage.getItem("pcount");
-        txt = "icy = " + localStorage.getItem("icount");
+        pota = "potato = " + ponum;
+        txt = "icy = " + icy;
+        ocument.getElementById("yoy").innerHTML = ah;
+        document.getElementById("convert").innerHTML = txt;
+        document.getElementById("buyPo").innerHTML = pota;
       } else {
-        pota = "potato = " + localStorage.getItem("pcount");
-        txt = "icy = " + localStorage.getItem("icount");
+        pota = "potato = " + ponum;
+        txt = "icy = " + icy;
+        ocument.getElementById("yoy").innerHTML = ah;
+        document.getElementById("convert").innerHTML = txt;
+        document.getElementById("buyPo").innerHTML = pota;
       }
     }
   } else {
     alert("...");
     if (pota > 0) {
-      pota = "potato = " + localStorage.getItem("pcount");
-      txt = "icy = " + localStorage.getItem("icount");
+      pota = "potato = " + ponum;
+      txt = "icy = " + icy;
+      ocument.getElementById("yoy").innerHTML = ah;
+      document.getElementById("convert").innerHTML = txt;
+      document.getElementById("buyPo").innerHTML = pota;
     } else {
-      pota = "potato = " + localStorage.getItem("pcount");
-      txt = "icy = " + localStorage.getItem("icount");
+      pota = "potato = " + ponum;
+      txt = "icy = " + icy;
+      localStorage.setItem("pcount", ponum);
+      ocument.getElementById("yoy").innerHTML = ah;
+      document.getElementById("convert").innerHTML = txt;
+      document.getElementById("buyPo").innerHTML = pota;
     }
   }
+  document.getElementById("yoy").innerHTML = ah;
   document.getElementById("buyPo").innerHTML = pota;
   document.getElementById("convert").innerHTML = txt;
 }
@@ -137,3 +152,10 @@ function erase() {
     location.reload();
   }
 }
+//coding is like baking while blindfolded but also you have to make sure the oven knows how to turn on and the cupcake batter is real before you put it in the oven
+function makeDark() {
+  var element = document.body;
+  element.classList.toggle("accessib");
+}
+
+//coding is like baking while blindfolded but also you have to make sure the oven knows how to turn on and the cupcake batter is real before you put it in the oven
